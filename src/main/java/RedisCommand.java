5bb2c8b0509fa20ea0a6fce726caa8ed64a821e8
Kotaro Noguchi <kotaro.noguchi@linecorp.com>
2025-06-20 22:33:07 +0900

    Rough impl for ping and echo

---
+++ src/main/java/RedisCommand.java
@@ -0,0 +1,4 @@
+public enum RedisCommand {
+    PING,
+    ECHO,
+}
--- src/main/java/RedisServerHandler.java
+++ src/main/java/RedisServerHandler.java
@@ -1,3 +1,5 @@
+import org.apache.commons.lang3.NotImplementedException;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandler.Sharable;
@@ -7,10 +9,22 @@ import io.netty.util.CharsetUtil;
 
 @Sharable
 public class RedisServerHandler extends ChannelInboundHandlerAdapter {
+    private static String handle(String req) {
+        req = req.toLowerCase();
+        System.out.println(req);
+        if ("*1\r\n$4\r\nping\r\n".equals(req)) {
+            return "PONG";
+        } else if (req.startsWith("*2\r\n$4\r\necho\r\n")) {
+            return req.substring("*2\r\n$4\r\necho\r\n$1\r\n".length(), req.length() - 2);
+        }
+        throw new NotImplementedException("parse failed");
+    }
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ByteBuf in = (ByteBuf) msg;
-        ByteBuf response = Unpooled.copiedBuffer("+PONG\r\n", CharsetUtil.UTF_8);
+        System.out.println("in: " + in.toString(CharsetUtil.UTF_8));
+        ByteBuf response = Unpooled.copiedBuffer("+" + handle(in.toString(CharsetUtil.UTF_8)) + "\r\n", CharsetUtil.UTF_8);
         ctx.writeAndFlush(response);
     }
 
